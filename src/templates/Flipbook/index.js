375b960e8c5ff2d84419e05437f160aeff182924
trig-vee <tnordberg@smm.org>
2021-12-07 17:55:19 -0600

    feat: mapping slides query to swipeable slieshow

--- src/templates/Flipbook/index.js
+++ src/templates/Flipbook/index.js
@@ -1,22 +1,57 @@
+/* eslint no-console: 0 */
 import React from 'react';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { renderRichText } from 'gatsby-source-contentful/rich-text';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
 
 export const pageQuery = graphql`
   query ($slug: String!) {
     contentfulFlipbook(slug: { eq: $slug }) {
-        slug
+      slides {
+        id
+        title
+        body {
+          raw
+        }
+        media {
+          credit
+          altText {
+            altText
+          }
+          media {
+            gatsbyImageData(
+              width: 250
+              layout: FIXED
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
     }
   }
 `;
 
 const Flipbook = ({ data }) => {
   const { contentfulFlipbook } = data;
-  const { slug } = contentfulFlipbook;
+  const { slides } = contentfulFlipbook;
+
+  const renderSlides = slides.map((slide) => (
+    <SwiperSlide key={slide.id}>
+      <h1>{slide.title}</h1>
+      {renderRichText(slide.body)}
+      <GatsbyImage
+        image={getImage(slide.media.media)}
+        alt={slide.media.altText || 'Image'}
+        loading="eager"
+      />
+      <pre>{slide.media.credit}</pre>
+    </SwiperSlide>
+  ));
 
   return (
     <>
@@ -25,13 +60,7 @@ const Flipbook = ({ data }) => {
         slidesPerView={1}
         centeredSlides
       >
-        <SwiperSlide>
-          Slide 1--
-          {slug}
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {renderSlides}
       </Swiper>
     </>
   );
